e8958f4f1e22cf8bf918a6cfa4c58ceeb9cd44eb
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-05-16 10:17:11 +0000

    completo

--- sito.py
+++ sito.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 quartieri = gpd.read_file('Quartieri/NIL_WM.dbf')
 quartieri3857 = quartieri.to_crs(3857)
 
-df = pd.read_csv('sosta_turistici.csv')
+df = pd.read_csv('sosta_turistici.csv', sep = ";")
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
 geometry = [Point(xy) for xy in zip(df["LONG_X_4326"], df["LAT_Y_4326"])]
@@ -29,28 +29,37 @@ def home():
 @app.route('/esercizio1')
 def es1():
     quartiere = request.args.get('quartiere')
-    return render_template("mappa.html")
+    quartiereSelezionato = quartieri3857[quartieri3857.NIL == quartiere]
+    parcheggiQuartiereSelezionato = parcheggi3857[parcheggi3857.within(quartiereSelezionato.geometry.item())]
+    ax = quartiereSelezionato.plot(figsize = (17, 12), edgecolor = 'k', facecolor = 'none')
+    parcheggiQuartiereSelezionato.plot(ax = ax, markersize = 25, color = 'red')
+    ctx.add_basemap(ax)
+
+    dir = "static/images"
+    file_name = "es1.png"
+    save_path = os.path.join(dir, file_name)
+    plt.savefig(save_path, dpi = 150)
+    return render_template("mappa.html", immagine = file_name)
 
 
 
+@app.route('/esercizio2')
+def es2():
+    longitudine = float(request.args.get('longitudine'))
+    latitudine = float(request.args.get('latitudine'))
+    from shapely.geometry import Point
+    punto = gpd.GeoSeries([Point(longitudine, latitudine)], crs = 4326).to_crs(3857)
+    m500 = parcheggi3857[parcheggi3857.distance(punto.geometry.item()) < 500]
+    quartieriParcheggi500 = quartieri3857[quartieri3857.intersects(m500.unary_union)]
+    ax = m500.plot(figsize = (17, 12), markersize = 40, color = 'red')
+    quartieriParcheggi500.plot(ax = ax, edgecolor = 'k', facecolor = 'none', linewidth = 2)
+    ctx.add_basemap(ax)
 
-
-
-
-#dir = "static/images"
-#file_name = "es8-9-10.png"
-#save_path = os.path.join(dir, file_name)
-#plt.savefig(save_path, dpi = 150)
-
-
-
-
-
-
-
-
-
-
+    dir = "static/images"
+    file_name = "es2.png"
+    save_path = os.path.join(dir, file_name)
+    plt.savefig(save_path, dpi = 150)
+    return render_template("mappa.html", immagine = file_name)
 
 
 
